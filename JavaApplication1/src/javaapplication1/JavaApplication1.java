fa19209fc620d1400582c79f8266eaed38d4f82b
Matixz <matixz.kom@gmail.com>
2019-03-13 17:29:55 +0100

    2

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -15,7 +15,7 @@ public class JavaApplication1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("master");
+        System.out.println("branchh");
         // TODO code application logic here
     }
     
